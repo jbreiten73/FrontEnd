0e96ad9319a7e1de67cff87d161fb2a40ca7953a
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-09 16:25:09 +0200

    Update random.js

--- javascript/random.js
+++ javascript/random.js
@@ -1,1 +1,13 @@
+  $("#new-quote").click(genRandQuote);
+function genRandQuote(){
+     let quotes=[{text:"Manches können die Menschen besser, und 
+     manches die Skunks",source:"Mephitis"},
+{text:"Das Gegenteil einer großen Wahrheit ist eine andere große Wahrheit",
+source:"Niels Bohr"},
+{text:"Ich weiß,dass ich nichts weiß",source:"Sokrates"},
+{text:"Wer ohne Sünde ist, werde den ersten Stein",
+source:"Jesus"}];
 
+  var randQuote = quotes[Math.floor(Math.random() * (quotes.length))];
+  $("#text").html(randQuote.text);
+  $("#author").html(randQuote.source);};
