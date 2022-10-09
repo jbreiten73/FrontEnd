e54df00641658a3a3150c38c2290c9d45e2dfe8f
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-09 20:13:39 +0200

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -11,13 +11,19 @@ function go() {
 
   
 function genRandQuote(){
-  var quotes=[{id:1,text:"Manches können die Menschen besser, und 
-     manches die Skunks",source:"Mephitis"},
-{id:2,text:"Das Gegenteil einer großen Wahrheit ist eine andere große Wahrheit",
+  var quotes=[{id:1,
+  text:"Manches können die Menschen besser, und 
+     manches die Skunks",
+   source:"Mephitis"},
+{id:2,
+text:"Das Gegenteil einer großen Wahrheit ist eine andere große Wahrheit",
 source:"Niels Bohr"},
-{id:3,text:"Ich weiß,dass ich nichts weiß",source:"Sokrates"},
-{id:4,text:"Wer ohne Sünde ist, werfe den ersten Stein",
-source:"Jesus"},];
+{id:3,
+text:"Ich weiß,dass ich nichts weiß",
+source:"Sokrates"},
+{id:4,
+text:"Wer ohne Sünde ist, werfe den ersten Stein",
+source:"Jesus"}];
 
   var randQuote = quotes[Math.floor(Math.random() * (quotes.length))];
   $("#text").html(randQuote.text);
