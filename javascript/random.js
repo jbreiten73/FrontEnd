d11ee855c88c717a738ac719b66187b3bed81c89
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-09 20:34:10 +0200

    Update random.js

--- javascript/random.js
+++ javascript/random.js
@@ -11,12 +11,12 @@ function go() {
 
   
 function genRandQuote(){
-  let quotes=[{id:1,text:"Manches können die Menschen besser, und 
+  let quotes=[{id:1,text:"Manches können die Menschen besser und 
      manches die Skunks",source:"Mephitis"},
 {id:2,text:"Das Gegenteil einer großen Wahrheit ist eine andere große Wahrheit",
 source:"Niels Bohr"},
-{id:3,text:"Ich weiß,dass ich nichts weiß",source:"Sokrates"},
-{id:4,text:"Wer ohne Sünde ist, werfe den ersten Stein",
+{id:3,text:"Ich weiß dass ich nichts weiß",source:"Sokrates"},
+{id:4,text:"Wer ohne Sünde ist werfe den ersten Stein",
 source:"Jesus"},];
 
   var randQuote = quotes[Math.floor(Math.random() * (quotes.length))];
