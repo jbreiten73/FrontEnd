0465bb65783c8214c3c044cec0bcd3097d6aab85
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-10 09:07:15 +0200

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,25 +1,19 @@
-
+window.go;
+function go(){
   // Triggers genRandQuote when New Quote button is clicked
   $("#new-quote").click(function() {
     genRandQuote();
   };
-  
-
+  genRandQuote();
+}
   
 function genRandQuote(){
-  var quotes=[{id:1,
-  text:"Manches können die Menschen besser, und 
-     manches die Skunks",
-   source:"Mephitis"},
-{id:2,
-text:"Das Gegenteil einer großen Wahrheit ist eine andere große Wahrheit",
-source:"Niels Bohr"},
-{id:3,
-text:"Ich weiß,dass ich nichts weiß",
-source:"Sokrates"},
-{id:4,
-text:"Wer ohne Sünde ist, werfe den ersten Stein",
-source:"Jesus"}];
+  var quotes=[{text: "Quote 1",
+source: "Source 1"},
+{text: "Quote 2", source: "Source 2"},
+{text: "Quote 3", source: "Source 3"},
+{text: "Quote 4", source: "Source 4"},
+{text: "Quote 5", source: "Source 5"}];
 
   var randQuote = quotes[Math.floor(Math.random() * (quotes.length))];
   $("#text").html(randQuote.text);
